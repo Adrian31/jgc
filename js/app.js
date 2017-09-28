bdd39d52d462998fac176925d27e334fc3be74a0
Adrian31 <adrian_carabetta@hotmail.com>
2017-09-28 15:26:01 +0800

    bug fix attempt where right side isn't adjusting with query selector

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 //Empty
+
+var bodyCheck = function(){
+  console.log(document.body.clientWidth);
+  if (document.body.clientWidth > 650) {
+    console.log("hello");
+      $('#menu2').removeClass('open2');
+    }
+}
+
+bodyCheck();
+
+new ResizeSensor(jQuery(document.body.clientWidth), function(){
+    console.log('content dimension changed');
+});
