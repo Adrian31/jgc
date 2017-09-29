fc9bb32b4840f1f26832375384a70456e8df5475
Adrian <adrian_carabetta@hotmail.com>
2017-09-29 12:26:30 +0800

    Added li dropdown instead of static list

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 //Empty
+$bottom = $('.bottom');
+
+
+
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+}
+
+
+window.onload = function() {
+document.getElementById("showPDF").onclick = function() {
+    document.getElementById("thePDF").style.visibility = "visible";
+}
+}
